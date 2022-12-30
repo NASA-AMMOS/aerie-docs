0c043f712bc79b7fd758d52d6455bd59c999c8fb
camargo <camargo.cac@gmail.com>
2022-12-29 20:45:38 -0800

    Add trailing slash to redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 
 export default function Index() {
-  return <Redirect to="/aerie-docs/introduction" />;
+  return <Redirect to="/aerie-docs/introduction/" />;
 }
