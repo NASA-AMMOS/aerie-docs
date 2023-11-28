090bc9248edf17e2b83ef14ff93e06489e8119cf
Lyle Klyne <lyle.klyne@gmail.com>
2023-11-28 13:04:31 -0800

    add feature cards

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,19 +4,9 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Extensible mission modeling and simulation',
-    Svg: require('@site/static/img/aerie-feature-mission-modeling-light.svg').default,
-    size: 'col--4',
-    description: (
-      <>
-        Model your system with access to the entire Java ecosystem. Validate plans against your models using Aerie's
-        discrete event simulator using the user interface or Aerie API.
-      </>
-    ),
-  },
-  {
     title: 'Seamless workflow from planning to commanding',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    // Svg: require('@site/static/img/aerie-feature-planning-commanding-light.svg').default,
+    Img: require('@site/static/img/aerie-feature-planning-commanding-light.png').default,
     size: 'col--8',
     description: (
       <>
@@ -26,9 +16,22 @@ const FeatureList = [
     ),
   },
   {
+    title: 'Extensible mission modeling and simulation',
+    // Svg: require('@site/static/img/aerie-feature-mission-modeling-light.svg').default,
+    Img: require('@site/static/img/aerie-feature-mission-modeling-light.png').default,
+    size: 'col--4',
+    description: (
+      <>
+        Model your system with access to the entire Java ecosystem. Validate plans against your models using Aerie's
+        discrete event simulator using the user interface or Aerie API.
+      </>
+    ),
+  },
+  {
     title: 'Real-time collaboration and versioning',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    size: 'col--8',
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Img: require('@site/static/img/aerie-feature-collaboration-light.png').default,
+    size: 'col--4',
     description: (
       <>
         Create, modify, and test out plans together in real time from across the world. Or make branches, edit them
@@ -38,8 +41,9 @@ const FeatureList = [
   },
   {
     title: 'Low-code scheduling and rule checking',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
-    size: 'col--4',
+    // Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    Img: require('@site/static/img/aerie-feature-rule-checking-light.png').default,
+    size: 'col--8',
     description: (
       <>
         Automate plan creation with prioritized scheduling goals. Author constraints, evaluate them against a simulation
@@ -49,17 +53,16 @@ const FeatureList = [
   },
 ];
 
-function Feature({ Svg, title, description, size }) {
+function Feature({ Img, title, description, size }) {
   return (
     <div className={clsx(`col ${styles.featureSection}`, size)}>
       <div className={styles.featureCard}>
         <div className="card__header">
           <h3>{title}</h3>
         </div>
-        <div className="card__body">
+        <div className={clsx(`card__body ${styles.cardBody}`)}>
           <p>{description}</p>
-
-          <Svg className={styles.featureSvg} role="img" />
+          <img src={Img} className={styles.featureImg} alt={title} />
         </div>
       </div>
     </div>
