7e3e9db0af1afa95a721ed556bf902860016db82
Chet Joswig <cjoswig@yahoo.com>
2023-12-19 17:31:03 -0800

    Release v2.2.0 (#111)
    * Release v2.2.0
    
    * added note on sim config env var
    
    ---------
    
    Co-authored-by: joswig <Joseph.C.Joswig@jpl.nasa.gov>

--- sidebars.js
+++ sidebars.js
@@ -292,6 +292,7 @@ const sidebars = {
     'glossary',
   ],
   upgradeGuides: [
+    'upgrade-guides/2-1-0-to-2-2-0',
     'upgrade-guides/2-0-0-to-2-1-0',
     'upgrade-guides/1-14-0-to-2-0-0',
     'upgrade-guides/1-13-0-to-1-14-0',
