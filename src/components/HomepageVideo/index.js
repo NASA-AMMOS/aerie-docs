b66de8f6e2d279b8fff54d5db4a1d76af17802ca
Lyle Klyne <lyle.klyne@gmail.com>
2024-01-29 12:09:44 -0700

    fix how video is imported

--- src/components/HomepageVideo/index.js
+++ src/components/HomepageVideo/index.js
@@ -2,13 +2,17 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
+// TODO update this to our actual promo video when it clears review
+import PromoVideo from '@site/static/video/earth.mp4';
+
+
 export function HomepageVideo() {
   return (
     <section className={styles.section}>
       <div className="container">
         <div className={clsx(`row ${styles.videoSection}`)}>
           <video width="100%" height="auto" controls>
-            <source src="@site/static/video/earth.mp4" type="video/mp4" />
+            <source src={PromoVideo} type="video/mp4" />
           </video>
         </div>
       </div>
