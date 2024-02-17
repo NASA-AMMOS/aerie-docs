d3dcc41bead3c14e69a6b6efcc9c4a66873a0fda
Chet Joswig <cjoswig@yahoo.com>
2024-02-16 19:39:45 -0800

    Release v2.4.0 (#125)
    * Release v2.4.0
    
    * Update 2-3-0-to-2-4-0.md
    
    ---------
    
    Co-authored-by: joswig <Joseph.C.Joswig@jpl.nasa.gov>

--- sidebars.js
+++ sidebars.js
@@ -323,6 +323,7 @@ const sidebars = {
     'glossary',
   ],
   upgradeGuides: [
+    'upgrade-guides/2-3-0-to-2-4-0',
     'upgrade-guides/2-2-0-to-2-3-0',
     'upgrade-guides/2-1-0-to-2-2-0',
     'upgrade-guides/2-0-0-to-2-1-0',
