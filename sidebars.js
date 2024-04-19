0f54a363f433425972ae946f17e735e5f05da42f
Dan Delany <dan.delany@gmail.com>
2024-04-18 17:45:31 -0700

    minor updates to upgrade guides - make sure all patch versions are included & add note to 2.8.0 (#145)

--- sidebars.js
+++ sidebars.js
@@ -326,7 +326,8 @@ const sidebars = {
   upgradeGuides: [
     'upgrade-guides/2-7-1-to-2-8-0',
     'upgrade-guides/2-7-0-to-2-7-1',
-    'upgrade-guides/2-6-1-to-2-7-0',
+    'upgrade-guides/2-6-2-to-2-7-0',
+    'upgrade-guides/2-6-1-to-2-6-2',
     'upgrade-guides/2-6-0-to-2-6-1',
     'upgrade-guides/2-5-0-to-2-6-0',
     'upgrade-guides/2-4-0-to-2-5-0',
