a1cf1aa813faf98ad6b773136a83445d31ade2f5
Ferguson, Eric W (397B) <Eric.W.Ferguson@jpl.nasa.gov>
2024-08-02 15:30:41 -0700

    Add definitions for project repo statuses. Add repo status to docs repo

--- sidebars.js
+++ sidebars.js
@@ -333,6 +333,16 @@ const sidebars = {
         },
       ],
     },
+    {
+      type: 'category',
+      label: 'Aerie Ecosystem',
+      link: {
+        type: 'generated-index',
+      },
+      items: [
+        'ecosystem/repositories/repos-introduction'
+      ],
+    },
     'glossary',
     {
       label: 'Keyboard Shortcuts',
