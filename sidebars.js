4400009122750ccadb867d3158832aae32abbd62
joswig <Joseph.C.Joswig@jpl.nasa.gov>
2025-08-11 23:00:06 +0000

    Release v3.5.2

--- sidebars.js
+++ sidebars.js
@@ -507,6 +507,7 @@ const sidebars = {
     },
   ],
   upgradeGuides: [
+    'upgrade-guides/3-5-1-to-3-5-2',
     'upgrade-guides/3-5-1-to-3-6-0',
     'upgrade-guides/3-5-0-to-3-5-1',
     'upgrade-guides/3-4-0-to-3-5-0',
